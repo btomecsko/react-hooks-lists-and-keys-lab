20945e23a2029113d2d5c2e7e8814391057b6f0a
btomecsko <brandon.tomecsko92@gmail.com>
2022-08-30 21:20:12 -0500

    passed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linkMap = links.map( x => {
+    return <a key = {x} href= {"#"+x}> {x}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{linkMap}</nav>;
 }
 
 export default NavBar;
